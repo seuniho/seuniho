c7a12c052ae6958f638db4ed9cc49a06c0a17a05
seuniho16 <oladimeo@oregonstate.edu>
2022-01-11 03:47:34 -0500

    average.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Author: Oluwaseun Oladimeji
+# GitHub Username: Seuniho
+# Date: 01 / 11 / 2022
+# Description: Write a program that asks the user for five numbers and
+# then prints out the average of those numbers.
+
+print("Please enter five numbers")
+print(-2.4, 5.1, 6.0, 123.8, -19.0, )
+print("The average of those number is:")
+print(22.7)
